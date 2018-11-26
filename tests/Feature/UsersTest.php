4ce99723ed0555b2ecf2d85db5131b7eb1f1555f
Rossi <admin@rassloff.info>
2018-11-26 19:05:57 +0100

    User Test

--- tests/Feature/UsersTest.php
+++ tests/Feature/UsersTest.php
@@ -5,6 +5,8 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
+use App\User;
+
 class UsersTest extends TestCase
 {
     /**
@@ -19,10 +21,43 @@ class UsersTest extends TestCase
         $response->assertStatus(200);
     }
 
+    /**
+     *  GET all users
+     */
     public function testUsersTest()
     {
         $response = $this->get('/users');
 
         $response->assertStatus(200);
+    }
+
+    /**
+     *    POST  user without Header and Auth
+     */
+    public function testPostUserTest()
+    {
+        $this->withoutMiddleware();
+
+        $response = $this->post('/users/', [
+            'email'    => 'user' . rand(0, 99999999) . '@gmail.com',
+            'name'     => 'Billy ' . rand(0, 99999999) . ' Joel',
+            'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        ]);
+
+        $response->assertStatus(201);
+
+    }
+
+    /**
+     *       DELETE a user
+     */
+    public function testEndpointContactsDelete(){
+
+        $this->withoutMiddleware();
+
+        $user = factory(User::class)->create([]);
+        $response = $this->delete('/users/' . $user->id);
+
+        $response->assertStatus(204);
     }
 }
