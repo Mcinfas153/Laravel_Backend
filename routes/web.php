0550b2c6102ec8f42f016d48e572b428ff6e7944
Rossi <admin@rassloff.info>
2018-12-17 14:45:29 +0100

    some changes

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Route::get('/', function () {
  */
 Route::get('users', 'UserController@index');
 Route::get('users/{id}', 'UserController@show');
+
 //Route::post('users/{id}', 'UserController@update');
 //Route::post('users', 'UserController@store');
 //Route::delete('users/{id}', 'UserController@destroy');
